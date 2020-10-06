bca24262e73eb5028ee0a89dd713af6e2cbb49cb
Vitor Figueiredo <vitorfigueiredo855@gmail.com>
2020-10-05 21:53:17 -0300

    adiciona o pdf com as questões da lista de exercicios

---
+++ lista-exercicios-2/src/Questao1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Questao1 {
+
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		String nome_completo, primeiro_nome, ultimo_nome;
+		
+		nome_completo = "Vitor Figueiredo da Silva";
+		System.out.println("Nome Completo: " + nome_completo);
+		
+		primeiro_nome = nome_completo.substring(0, nome_completo.indexOf(" "));
+		ultimo_nome = nome_completo.substring(nome_completo.lastIndexOf(" "));
+		
+		System.out.println("Primeiro e último nome: " + primeiro_nome + ultimo_nome);
+		
+		teclado.close();
+	}
+}
